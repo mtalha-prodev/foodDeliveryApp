9caa772a9992fd09abec4be6bdbcdedade06ff9b
pny999 <pnyaitlecture@gmail.com>
2023-02-10 12:44:24 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = ()=>{
+  return(
+    <View>
+      <Text>Welcome Food Delivery App</Text>
+    </View>
+  )
+}
+export default App;
