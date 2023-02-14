e499dc3c832ea31bd89bf654461925f1824bb03e
pny999 <pnyaitlecture@gmail.com>
2023-02-14 23:51:18 +0500

    get items data in firestore

--- src/admin/tabs/Item.js
+++ src/admin/tabs/Item.js
@@ -1,12 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet, Image, Button} from 'react-native';
+import React,{useState} from 'react';
+import firestore from '@react-native-firebase/firestore';
 
 const Item = () => {
-  return (
-    <View>
-      <Text>Item</Text>
-    </View>
-  )
-}
+  const [data, setData] = useState(null)
 
-export default Item
+  const getAllItems = ()=>{
+    try {
+        firestore().get().then((data)=>{
+          console.log(data)
+          data.forEach(itmes => {
+            console.log(`userId: ${itmes.id} : ${itmes.data}`)
+          });
+        })
+      // console.log(response._data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  return (
+    <View style={style.constiner}>
+      <View style={style.cartBox}>
+        {/* <Image source={require()} /> */}
+        <View style={style.cartDetail}>
+          <Text>name</Text>
+          <Text>modal</Text>
+          <View>
+            <Text>price</Text>
+            <Text>discount Price</Text>
+          </View>
+        </View>
+        <View style={style.cartAction}>
+          <Text>Delete</Text>
+          <Text>Add Card</Text>
+        </View>
+      </View>
+      <Button title='Get Items' onPress={()=>getAllItems()} />
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  constiner: {
+    flex: 1,
+  },
+  cartBox: {
+    width: '90%',
+    height: 100,
+    elevation: 4,
+    alignSelf: 'center',
+    marginVertical: 10,
+    backgroundColor: '#fff',
+    padding: 5,
+    borderRadius: 8,
+    flexDirection:'row'
+  },
+});
+
+export default Item;
